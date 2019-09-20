e64d033687b48ac1501b079196bb232a443059f1
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-20 10:26:48 -0500

    retrieved token from server

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+const LoginForm = (props) => {
 
-export default Login;
+const [ creds, setCreds] = useState({ username: '', password: '' })
+   
+
+    const handleChange = e => {
+        e.preventDefault();
+        setCreds({ ...creds,  [e.target.name]: e.target.value });
+        console.log(e.target.name)
+    }
+
+    const login = e => {
+        e.preventDefault();
+        axiosWithAuth()
+        .post('/login', creds)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/colors')
+            console.log(res)
+        })
+        .catch(err => {
+            console.log('my err', err)
+            console.log(creds)
+        })
+
+        console.log(creds)
+    }
+
+    return (
+        <>
+        <h1>Welcome to the Bubble App!</h1>
+        <form className='bubble-form' onSubmit={login}>
+            <input
+            type='text'
+            value={creds.username}
+            placeholder='name'
+            onChange={handleChange}
+            name='username'
+            >
+            </input>
+            <input
+            type='password'
+            value={creds.password}
+            placeholder='password'
+            onChange={handleChange}
+            name='password'
+            >
+            </input>
+            <button type='submit'>Log in</button>
+        </form>
+        </>
+    )
+}
+
+export default LoginForm;
